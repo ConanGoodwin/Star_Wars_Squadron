3b295cce4fe12e959311fee68eb2b649eaaa4a70
ConanGoodwin <conan_goodwin@hotmail.com>
2024-02-28 10:39:42 -0300

    chore: add more cards imgs

--- src/assets/updates/armas_secundarias/torpedos/index.js
+++ src/assets/updates/armas_secundarias/torpedos/index.js
@@ -1,11 +1,13 @@
 import MunicaoExtra from './Municao-extra.png';
 import TorpedosDardos from './Torpedos-dardos.png';
+import TorpedosIon from './Torpedos-ion.png'
 import TorpedoPlasma from './Torpedos-plasma.png';
 import TorpedoProton from './Torpedos-proton.png';
 
 export {
   MunicaoExtra,
   TorpedosDardos,
+  TorpedosIon,
   TorpedoPlasma,
   TorpedoProton,
 }
---
+++ src/assets/updates/sistemas/index.js
@@ -0,0 +1,9 @@
+import ComputadorMiraAvancada from './Computador-mira-avancada.png';
+import CorretorPrecisao from './Corretor-precisao.png';
+import SistemaControleTiro from './Sistema-controle-tiro.png';
+
+export {
+  ComputadorMiraAvancada,
+  CorretorPrecisao,
+  SistemaControleTiro,
+}
